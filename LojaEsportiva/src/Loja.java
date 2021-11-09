5d3851ca515c9ab3f5cd1328eaf7df998451572e
Bronin <brunownlobao@gmail.com>
2021-11-09 20:35:36 -0300

    Inicialização da branch Bruno

---
+++ LojaEsportiva/src/Loja.java
@@ -0,0 +1,18 @@
+
+public interface Loja {
+	
+	void cadastrarVendas();
+	void reabastecerEstoque();
+	void cadastrarCliente();
+	void excluirCliente();
+	void listarClientesCadastrados();
+	void listarClientesExcluidos();
+	void listarTodasAsVendas();
+	void listarComprasCliente();
+	void listarProdutosPorCategoria();
+	void listarProdutosCadastrados();
+	void listarProdutosExcluidos();
+	void listarProdutosForaDoEstoque();
+	void listarProdutosDisponiveis();
+
+}
---
+++ LojaEsportiva/src/LojaEsportiva.java
@@ -0,0 +1,84 @@
+
+public class LojaEsportiva implements Loja {
+	
+	
+
+	@Override
+	public void cadastrarVendas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reabastecerEstoque() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cadastrarCliente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluirCliente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarClientesCadastrados() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarClientesExcluidos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarTodasAsVendas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarComprasCliente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarProdutosPorCategoria() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarProdutosCadastrados() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarProdutosExcluidos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarProdutosForaDoEstoque() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarProdutosDisponiveis() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
