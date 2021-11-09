7158de00aafe1bd74e69f1404f1d49c93a069437
Bronin <brunownlobao@gmail.com>
2021-11-09 19:55:53 -0300

    Atualização do AppLoja

--- LojaEsportiva/src/AppLoja.java
+++ LojaEsportiva/src/AppLoja.java
@@ -1,9 +1,41 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
 
 public class AppLoja {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private JFrame frame;
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppLoja window = new AppLoja();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AppLoja() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
